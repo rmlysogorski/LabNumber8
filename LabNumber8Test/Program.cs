ed8c210f9e15b7de6bf67163842b64b518d98d8d
Rich Lysogorski <rmlysogorski@gmail.com>
2019-04-30 18:49:13 -0400

    Grand Circus C# .NET April 2019 Lab Number 8

---
+++ LabNumber8Test/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber8Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            UserInput U = new UserInput();
+            //int holds student ID starting with 1
+            //string[] holds different info at different indicies:
+            //0 = name, 1 = hometown, 2 = favorite food
+            //the data can be acessed with GetStudentInfo(studentID, "name"/"hometown"/"favorite food", dictionary)
+            Dictionary<int, string[]> studentList = U.RetrieveData();
+
+            string welcomeMessage = "Welcome to our C# class.";
+            string studentPrompt = $"Which student would you like to learn more about? (enter a number 1-{studentList.Count}): ";
+            bool IDisGood = false, learnAboutStudent = true, moreInfo = true;
+
+            UserInput.PrintInColor(welcomeMessage + "\n", ConsoleColor.Green);            
+            
+            while (learnAboutStudent)
+            {                
+                moreInfo = true;
+                int studentID = U.GetValidInt(studentPrompt);
+                IDisGood = U.CheckStudentID(studentID, studentList.Count);
+                if (IDisGood)
+                {
+                    UserInput.PrintInColor($"Student {studentID} is {U.GetStudentInfo(studentID, "name", studentList)}.\n", ConsoleColor.Yellow);
+                    while (moreInfo)
+                    {
+                        Console.WriteLine($"What would you like to know about {U.GetStudentInfo(studentID, "name", studentList).Split()[0]}?");
+                        string userInput = U.GetString("Please enter \"hometown\" or \"favorite food\": ");
+                        UserInput.PrintInColor($"{U.GetStudentInfo(studentID, "name", studentList).Split()[0]}'s " +
+                            $"{userInput} is {U.GetStudentInfo(studentID, userInput, studentList)}.\n", ConsoleColor.Yellow);
+                        moreInfo = EndProgram("Would you like to know more about " +
+                            $"{U.GetStudentInfo(studentID, "name", studentList).Split()[0]}? (y/n)", "y", "n");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"Student {studentID} does not exist.");
+                }
+
+                learnAboutStudent = EndProgram("Would you like to learn about a different student? (y/n) :", "y", "n");
+            }            
+            
+        }
+
+        public static bool EndProgram(string message, string option1, string option2)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+
+            if (input == option1)
+            {
+                return true;
+            }
+            else if (input == option2)
+            {
+                return false;
+            }
+            else
+            {
+                UserInput.PrintInColor("Please enter 'y' for yes and 'n' for no.\n", ConsoleColor.Red);
+                return EndProgram(message, option1, option2);
+            }
+        }
+    }
+}
---
+++ LabNumber8Test/UserInput.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber8Test
+{
+    class UserInput
+    {
+
+        public Dictionary<int,string[]> RetrieveData()
+        {
+            //This function creates a dictionary by reading a file
+            //that was created with a separate program and
+            //serialized with a binary formatter
+            try
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                FileInfo fileInfo = new FileInfo(@"studentList.bin");
+
+                Dictionary<int, string[]> studentList;
+                using (var binaryFile = fileInfo.OpenRead())
+                {
+                    studentList = (Dictionary<int, string[]>)binaryFormatter.Deserialize(binaryFile);
+                }
+
+                return studentList;
+
+            }
+            catch (IOException E)
+            {
+                Console.WriteLine(E.Message);
+                Dictionary<int, string[]> defaultDict = new Dictionary<int, string[]>();
+                return defaultDict;
+            }
+        }
+
+        public static void PrintInColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        public int GetValidInt(string message)
+        {
+            Console.Write(message);
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                return input;
+            }
+            catch(FormatException E)
+            {
+                PrintInColor(E.Message + "\n", ConsoleColor.Red);
+                return GetValidInt(message);
+            } 
+        }
+
+        public bool CheckStudentID(int ID, int dictLength)
+        {
+            try
+            {
+                if(ID > dictLength || ID < 1)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return true;
+            }
+            catch(IndexOutOfRangeException I)
+            {
+                PrintInColor(I.Message + " Invalid input detected.\n", ConsoleColor.Red);
+                return false;
+            }
+        }
+
+        public string GetStudentInfo(int key, string index, Dictionary<int, string[]> dict)
+        {
+            switch (index)
+            {
+                case "name": return dict[key][0];
+
+                case "hometown": return dict[key][1];
+
+                case "favorite food": return dict[key][2];
+
+                default: return "";
+                                    
+            }            
+        }
+
+        public string GetString(string message = "Please enter a string: ", string error = "Error - oops, it looks like you forgot to enter something!\n")
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                PrintInColor(error, ConsoleColor.Red);
+                return GetString(message, error);
+            }
+            else if (input == "hometown")
+            {
+                return input;
+            }
+            else if (input == "favorite food")
+            {
+                return input;
+            }
+            else
+            {
+                PrintInColor("That data does not exist.\n", ConsoleColor.Red);
+                return GetString(message,error);   
+            }
+        }
+    }
+}
